c4406c448cf5fe5b5d2e6bddc3ce2acb71f9d600
Owen Wardlaw <owardlaw@ucsd.edu>
2022-12-08 13:01:57 -0800

    Adding files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Electron Website 
+
+**Quickly create a working desktop application from a already hosted website.**
+
+## Installation
+
+```bash
+npm i
+
+yarn
+```
+
+## Usage
+
+```bash
+npm start
+
+yarn start
+```
---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron')
+require('@electron/remote/main').initialize()
+
+function createWindow() {
+
+  // checking os
+  if (process.platform === "win32") {
+    var iconPath = 'icons/icon.png';
+  } else if (process.platform === "darwin") {
+    var iconPath = 'icons/icon.icns';
+  }
+
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    },
+    icon: iconPath
+  })
+
+  // loading main window
+  win.loadURL(`http://youtube.com/`);
+  win.show();
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
